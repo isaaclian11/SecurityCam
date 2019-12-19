73c6729805cb3d62a3139ddcf157403156d06e2c
Lucas Jedlicka <jedlicka@iastate.edu>
2019-12-19 15:27:05 -0600

    resize video, allow updating via textbox

--- app/src/main/java/com/isanga/securitycam/Activities/ViewerActivity.java
+++ app/src/main/java/com/isanga/securitycam/Activities/ViewerActivity.java
@@ -8,17 +8,25 @@ import android.content.SharedPreferences;
 import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.isanga.securitycam.R;
 
 import java.io.IOException;
+import java.net.URI;
 
 
 public class ViewerActivity extends AppCompatActivity
@@ -27,7 +35,8 @@ public class ViewerActivity extends AppCompatActivity
     public final static String TAG = "ViewerActivity";
 
     private VideoView videoView;
-
+    private EditText ipText;
+    private Uri uri;
 
 
     @Override
@@ -38,14 +47,52 @@ public class ViewerActivity extends AppCompatActivity
         setContentView(R.layout.activity_viewer);
 
         videoView = (VideoView) findViewById(R.id.videoView);
-        Uri uri = Uri.parse("rtsp://192.168.15.140:8554/test.ts");
-        try {
-            videoView.setVideoURI(uri);
-            videoView.start();
-        }catch (Exception ex ){
+        uri = Uri.parse("rtsp://10.26.40.54:8554/test.ts");
+        updateVideo(uri);
 
-        }
 
+        ipText = (EditText) findViewById(R.id.editTextIP);
+
+        ipText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_DONE) {
+                    Uri fresh = Uri.parse(textView.getText().toString());
+                    if (fresh != null) {
+                        updateVideo(fresh);
+                        Log.d(TAG, "onEditorAction: " + fresh.toString());
+                    } else {
+                        Toast.makeText(ViewerActivity.this, "Invalid URI", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        videoView.suspend();
+    }
+
+    private void updateVideo(Uri uri){
+        this.uri = uri;
+        videoView.stopPlayback();
+        videoView.suspend();
+        videoView.setVideoURI(uri);
+        videoView.start();
+        resizeVideo();
+    }
+
+    private void resizeVideo(){
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        android.widget.RelativeLayout.LayoutParams params = (android.widget.RelativeLayout.LayoutParams) videoView.getLayoutParams();
+        params.width = metrics.widthPixels;
+        params.height = metrics.heightPixels;
+        params.leftMargin = 0;
+        videoView.setLayoutParams(params);
     }
 
 
