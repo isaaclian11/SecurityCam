0042aa3c01cd2702bed667db8e3c99525a97002f
unknown <isaaclian11@gmail.com>
2019-12-19 21:43:56 -0600

    Updated User UI

--- app/src/main/java/com/isanga/securitycam/Fragments/User.java
+++ app/src/main/java/com/isanga/securitycam/Fragments/User.java
@@ -12,12 +12,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -62,15 +64,15 @@ public class User extends Fragment {
     /**
      * Sign in button
      */
-    private Button signinBtn;
+    private SignInButton signinBtn;
     /**
      * Sign out button
      */
-    private Button signoutBtn;
+    private ImageView signoutBtn;
     /**
      * Sync button
      */
-    private Button syncBtn;
+    private ImageView syncBtn;
 
     /**
      * Current userID
